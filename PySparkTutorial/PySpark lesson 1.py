284ce4450a2d491fafd8547b060f7d908a21acd2
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-05-28 15:44:18 -0400

    updated commit
    Added code to convert pandas data frame to spark data frame

--- PySparkTutorial/PySpark lesson 1.py
+++ PySparkTutorial/PySpark lesson 1.py
@@ -1,6 +1,8 @@
 #this code will run only when run on the pyspark201 interpreter in pycharm
 import pyspark
 from pyspark.sql import SparkSession
+import pandas as pd
+import numpy as np
 
 sc = pyspark.SparkContext("local","PySpark lesson 1")
 
@@ -10,5 +12,26 @@ spark = SparkSession.builder.getOrCreate()
 #printing to console
 print(spark)
 
-#viewing the tables within the spark context
-print(spark.catalog.listTables())
+#creating a pandas dataframe with 10 random numbers
+df_temp = pd.DataFrame(np.random.random(10))
+
+#creating a spark dataframe using the createDataFrame() method with the df_temp as the argument.
+spark_temp = spark.createDataFrame(df_temp)
+print(spark_temp.head())
+
+#checking the list of tables in the catalog before adding the spark_temp to the catalog
+if spark.catalog.listTables() == []:
+    print('empty catalog')
+else:
+    print(spark.catalog.listTables())
+
+#adding the spark_temp data frame to the catalog
+spark_temp.createOrReplaceTempView('spark_temp')
+
+#checking if the spark_temp data frame got added to the catalog
+if spark.catalog.listTables() == []:
+    print('empty catalog')
+else:
+    print(spark.catalog.listTables())
+
+
